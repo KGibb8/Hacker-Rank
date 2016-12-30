649fdfffb1b9a7f40ad7ed6d0e7b625c78c27e4d
Kieran Gibb <kieran.a.gibb@gmail.com>
2016-12-30 23:00:56 +0000

    Solved Security Functions II

---
+++ security/functions/security-functions-II.rb
@@ -0,0 +1,9 @@
+# Your task is to complete the function that takes as the input x and return x*2.
+
+def function(x)
+  x ** 2
+end
+
+# This is an example of an injective function.
+# It never maps distinct elements of its domain to the same element in its codomain.
+# Every element of the functions codomain is an image of at most one element of its domain.
